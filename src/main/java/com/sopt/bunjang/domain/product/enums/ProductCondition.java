c8d822796da0abc30162c74c4661207f0523d654
yujeong <parkyj0430@gmail.com>
2026-05-11 16:10:52 +0900

    feat(#1/common): enum값 정의

---
+++ src/main/java/com/sopt/bunjang/domain/product/enums/ProductCondition.java
@@ -0,0 +1,9 @@
+package com.sopt.bunjang.domain.product.enums;
+
+public enum ProductCondition {
+    NEW,            // 새 상품
+    LIKE_NEW,       // 사용감 없음
+    LIGHTLY_USED,   // 사용감 적음
+    HEAVILY_USED,   // 사용감 많음
+    BROKEN          // 고장/파손
+}
