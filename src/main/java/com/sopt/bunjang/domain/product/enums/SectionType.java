ef7f4aadcf3e0f987e6b7d64cbdcc6a08dd7fed7
yujeong <parkyj0430@gmail.com>
2026-05-13 14:54:05 +0900

    feat(#1/common): SectionType에 '이 상품과 함께 많이 구매한 상품' enum값 추가

--- src/main/java/com/sopt/bunjang/domain/product/enums/SectionType.java
+++ src/main/java/com/sopt/bunjang/domain/product/enums/SectionType.java
@@ -5,5 +5,6 @@ public enum SectionType {
     SIMILAR_PRODUCT,    // 최근 본 상품과 비슷한 상품
     AD,                 // 광고
     RECOMMENDED,        // 좋아할 만한 상품
-    RELATED_STYLE       // 같은 스타일의 다른 상품
+    RELATED_STYLE,      // 같은 스타일의 다른 상품
+    BOUGHT_TOGETHER     // 이 상품과 함께 많이 구매한 상품
 }
